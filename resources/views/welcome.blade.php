1df10020203288da9ff3ff48fe7e3805f88e82cf
Raihan Ihsan Bagaskoro <s160414061@student.ubaya.ac.id>
2019-11-27 00:05:12 +0700

    second commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Hello Worlds
                 </div>
 
                 <div class="links">
